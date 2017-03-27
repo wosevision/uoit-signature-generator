ae17097bdc7682464668aed9237518bbd15e8825
Wose <Jacksonteather@hotmail.com>
2017-03-27 13:08:33 -0400

    ☝️ Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+angular-through-iframe
+======================
+
+How to use angular.js through an iframe. See the [live demo here](http://james2doyle.github.io/angular-through-iframe/).
+
+Example:
+
+![angular iframe example](https://raw.githubusercontent.com/james2doyle/angular-through-iframe/master/angular-iframe.gif)
---
+++ send.php
@@ -0,0 +1,73 @@
+<?php
+
+//SMTP needs accurate times, and the PHP time zone MUST be set
+//This should be done in your php.ini, but this is how to do it if you don't have access to that
+date_default_timezone_set('EST/UTC');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	// echo 'fun!';
+	$req = json_decode(file_get_contents('php://input'),true);
+	// foreach ($req as $k => $v) {
+	// 	echo preg_replace('/\s+/S', " ", $v);
+	// }
+	$error = false;
+
+	require 'PHPMailer/PHPMailerAutoload.php';
+
+	//Create a new PHPMailer instance
+	$mail = new PHPMailer;
+	//Tell PHPMailer to use SMTP
+	$mail->isSMTP();
+	//Enable SMTP debugging
+	// 0 = off (for production use)
+	// 1 = client messages
+	// 2 = client and server messages
+	$mail->SMTPDebug = 0;
+	//Ask for HTML-friendly debug output
+	$mail->Debugoutput = 'html';
+	//Useful character encoding
+	$mail->CharSet = 'UTF-8';
+	//Set the hostname of the mail server
+	$mail->Host = "smtp-mail.outlook.com";
+	//Set the SMTP port number - likely to be 25, 465 or 587
+	$mail->Port = 587;
+	//Whether to use SMTP authentication
+	$mail->SMTPAuth = true;
+	//Username to use for SMTP authentication
+	$mail->Username = "jacksonteather@hotmail.com";
+	//Password to use for SMTP authentication
+	$mail->Password = "NastyJaspy1234";
+	//Set who the message is to be sent from
+	$mail->setFrom('signaturemailer@uoit.ca', 'Signature mailer');
+	//Set an alternative reply-to address
+	$mail->addReplyTo('webteam@uoit.ca', 'UOIT Webteam');
+
+	// $mail->isHTML(true);
+	//Set who the message is to be sent to
+	// $addressees = explode(',', trim($_POST['to']));//, 'John Doe');
+	//Set the subject line
+	$mail->Subject = '[do not reply] Your UOIT email signature';
+	//Read an HTML message body from an external file, convert referenced images to embedded,
+	//convert HTML into a basic plain-text alternative body
+	//Replace the plain text body with one created manually
+	$mail->Body = preg_replace('/\s+/S', ' ', $req['html'] );
+	$mail->AltBody = preg_replace('/\s+/', '', strip_tags($req['html']) );
+	//Attach an image file
+	//$mail->addAttachment('images/phpmailer_mini.png');
+	// foreach ($addressees as $a) {
+		$mail->addAddress($req['sendto']);
+		if (!$mail->send()) {
+			$error = $mail->ErrorInfo;
+		}
+		$mail->clearAddresses();
+	// }
+
+	//send the message, check for errors
+	if ($error) {
+	    echo '<br/><div data-alert class="alert-box alert"><strong style="font-family:franklin_gothic_fsdemi;">Mailer error: </strong>'.$error.'<a href="#" class="close">&times;</a></div><br/><p>DEBUG: '.$debug ;
+	} else {
+	    echo '<br/><div data-alert class="alert-box success"><strong style="font-family:franklin_gothic_fsdemi;">Success!</strong> Message sent.<a href="#" class="close">&times;</a></div>';
+	}
+	
+}
+
+?>
