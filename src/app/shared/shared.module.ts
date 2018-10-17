757cab1343abac66e22c070c28b302ac59d71009
Wose <Jacksonteather@hotmail.com>
2018-10-17 12:02:54 -0400

    🚿 Move absoluteUrlPipe to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,19 +4,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AccordionComponent } from './accordion/accordion.component';
 import { AccordionPanelComponent } from './accordion/accordion-panel.component';
+import { AbsoluteUrlPipe } from './absolute-url.pipe';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    BrowserAnimationsModule
-  ],
-  declarations: [
-    AccordionComponent,
-    AccordionPanelComponent,
-  ],
-  exports: [
-    AccordionComponent,
-    AccordionPanelComponent,
-  ]
+  imports: [CommonModule, BrowserAnimationsModule],
+  declarations: [AccordionComponent, AccordionPanelComponent, AbsoluteUrlPipe],
+  exports: [AccordionComponent, AccordionPanelComponent]
 })
-export class SharedModule { }
+export class SharedModule {}
--- src/app/signature-preview/signature-preview.module.ts
+++ src/app/signature-preview/signature-preview.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { SignaturePreviewComponent } from './signature-preview.component';
-import { AbsoluteUrlPipe } from './absolute-url.pipe';
 
 @NgModule({
-  imports: [
-    CommonModule,
-  ],
-  declarations: [
-    SignaturePreviewComponent,
-    AbsoluteUrlPipe,
-  ],
-  exports: [ SignaturePreviewComponent ]
+  imports: [CommonModule, SharedModule],
+  declarations: [SignaturePreviewComponent],
+  exports: [SignaturePreviewComponent]
 })
-export class SignaturePreviewModule { }
+export class SignaturePreviewModule {}
