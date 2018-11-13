4c78d123def8dc63c970bb6291aea2559d7ae745
Wose <Jacksonteather@hotmail.com>
2018-11-13 09:57:48 -0500

    🐛 Fix absolute URL pipe appending query params

--- src/app/shared/absolute-url.pipe.ts
+++ src/app/shared/absolute-url.pipe.ts
@@ -1,11 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import * as urlJoin from 'url-join';
 
-import { environment } from '../../environments/environment';
-
 @Pipe({ name: 'absoluteUrl' })
 export class AbsoluteUrlPipe implements PipeTransform {
   transform(value) {
-    return urlJoin(window.location.href, value);
+    const { protocol, host, pathname } = window.location;
+    return urlJoin(`${protocol}//${host}/${pathname}`, value);
   }
 }
